5161c969b7ce57d426f137fa12d9dcd2eb818f71
Igor da Rosa <igorlokox3@gmail.com>
2026-06-26 20:25:58 -0300

    menu

--- financaspy.py
+++ financaspy.py
@@ -12,3 +12,19 @@ if os.path.exists(ARQUIVO):
                     "data": partes[0], "tipo": partes[1],
                     "valor": float(partes[2]), "categoria": partes[3], "descricao": partes[4]
                 })
+
+# Menu
+
+while True:
+    print("\n--- MENU ---")
+    print("1 - REGISTRAR")
+    print("2 - VER EXTRATO")
+    print("3 - RELATÓRIO")
+    print("4 - EXPORTAR")
+    print("5 - SAIR")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        print("\n--- REGISTRAR ---")
+        tipo = input("Tipo (Receita/Despesa): ").strip().capitalize()
